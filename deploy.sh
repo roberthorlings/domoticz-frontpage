ac0888d565acf8c34111617e07c1bfd8ee27daa5
Robert Horlings <robert@isdat.nl>
2015-08-05 11:49:47 +0200

    Removed files not needed for deployment

--- deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-scp -r bootstrap custom dist plugins vendors *.html manifest.json pi@domoticz.local:/home/pi/domoticz/www/tablet
--- update.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-scp -r custom *.html manifest.json pi@domoticz.local:/home/pi/domoticz/www/tablet
